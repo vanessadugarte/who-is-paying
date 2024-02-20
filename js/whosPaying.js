ba7c88141efb1d0188e85990d8a88956b202b4f8
Vanessa Dugarte <vanessadugarte@MacBook-Pro-de-Vanessa.local>
2024-02-19 23:59:20 -0300

    adding delete button

--- js/whosPaying.js
+++ js/whosPaying.js
@@ -1,10 +1,17 @@
 let victimas = [];
+let contador = 0;
 
 function obtenerNombre() {
  
     var nombre = document.getElementById('idnombre').value;
     if(nombre !== "" && nombre !== undefined){
-        victimas.push(nombre);
+        contador++;
+        const nuevaVictima = {
+            id:contador,
+            nombre:nombre
+
+        }
+        victimas.push(nuevaVictima);
     console.log(victimas);
 
     document.getElementById('idnombre').value="";
@@ -22,10 +29,13 @@ function obtenerNombre() {
         let padreInput = document.createElement('article');
         padreInput.classList.add("padreInput_js")
         input.classList.add("inputVictimas_js")
+        input.dataset.id=victima.id;
+        eliminarA.onclick=function(){
+            eliminarVictima(victima.id);
+        };
         editarImg.classList.add("editar_js")
         eliminarImg.classList.add("eliminar_js")
-        eliminarA.dataset.eliminar = nombre;
-        input.value = victima;
+        input.value = victima.nombre;
         lista.appendChild(padreInput);
         padreInput.appendChild(input);
         editarA.href="#";
@@ -39,7 +49,6 @@ function obtenerNombre() {
         padreInput.appendChild(eliminarA);
         eliminarA.appendChild(eliminarImg);
         
-        
     })
 
     } else {
@@ -47,6 +56,50 @@ function obtenerNombre() {
     }
     
 }
+
+function eliminarVictima(id){
+    victimas = victimas.filter(victima=>victima.id !== id)
+    actualizar();
+}
+
+function actualizar() {
+    let lista = document.getElementById('listaVictimas');
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+    victimas.forEach(function(victima){
+        let input = document.createElement('input');
+        let editarA = document.createElement('a');
+        let editarImg = document.createElement('img');
+        let eliminarA = document.createElement('a');
+        let eliminarImg = document.createElement('img');
+        let padreInput = document.createElement('article');
+        padreInput.classList.add("padreInput_js")
+        input.classList.add("inputVictimas_js")
+        input.dataset.id=victima.id;
+        editarImg.classList.add("editar_js")
+        eliminarImg.classList.add("eliminar_js")
+        input.value = victima.nombre;
+        eliminarA.onclick=function(){
+            eliminarVictima(victima.id);
+        };
+        lista.appendChild(padreInput);
+        padreInput.appendChild(input);
+        editarA.href="#";
+        padreInput.appendChild(editarA);
+        editarImg.src="images/editar.svg";
+        editarA.href="#";
+        editarImg.alt="icono editar"
+        editarA.appendChild(editarImg);
+        eliminarImg.alt="icono eliminar"
+        eliminarImg.src="images/eliminar.svg";
+        padreInput.appendChild(eliminarA);
+        eliminarA.appendChild(eliminarImg);
+
+        
+    })
+}
+
 
 function seleccionarVictima() {
     let cantidadVictimas = victimas.length;
